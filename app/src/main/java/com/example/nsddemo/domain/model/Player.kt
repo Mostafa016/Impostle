3ae0fae6e28daf874bb91ad83b8b0b88ba2f6ed0
Mostafa Hesham <moselboss13@gmail.com>
2025-04-11 18:21:28 +0200

    Define serializable messages.
    - Use kotlinx serialization.

--- app/src/main/java/com/example/nsddemo/domain/model/Player.kt
+++ app/src/main/java/com/example/nsddemo/domain/model/Player.kt
@@ -1,3 +1,6 @@
 package com.example.nsddemo.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Player(val name: String, val color: String)
--- app/src/main/java/com/example/nsddemo/domain/util/Message.kt
+++ app/src/main/java/com/example/nsddemo/domain/util/Message.kt
@@ -2,37 +2,41 @@ package com.example.nsddemo.domain.util
 
 import com.example.nsddemo.domain.model.Player
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 @Serializable
 sealed class Message(private val messageOrder: MessageOrder) {
     val order: Int
         get() = messageOrder.ordinal
 
-    companion object {
-        val json = Json
-
-        fun toJson(message: Message): String = json.encodeToString(message)
-
-        inline fun <reified T : Message> fromJson(jsonString: String): Message =
-            json.decodeFromString<T>(jsonString)
-    }
+//    companion object {
+//        val json = Json
+//
+//        fun toJson(message: Message): String = json.encodeToString(message)
+//
+//        inline fun <reified T : Message> fromJson(jsonString: String): Message =
+//            json.decodeFromString<T>(jsonString)
+//    }
 }
 
-sealed class ServerMessage(messageOrder: MessageOrder) : Message(messageOrder) {
+@Serializable
+sealed class ServerMessage(private val globalOrder: MessageOrder) : Message(globalOrder) {
+    @Serializable
     data class PlayerColor(val playerColor: String) :
         ServerMessage(MessageOrder.SERVER_PLAYER_COLOR)
 
+    @Serializable
     data class IsLastPlayer(val isLastPlayer: Boolean) :
         ServerMessage(MessageOrder.SERVER_IS_LAST_PLAYER)
 
+    @Serializable
     data class PlayerList(val playerList: List<Player>) :
         ServerMessage(MessageOrder.SERVER_PLAYER_LIST)
 
+    @Serializable
     data class CategoryAndWord(val categoryOrdinal: Int, val wordResID: Int) :
         ServerMessage(MessageOrder.SERVER_CATEGORY_AND_WORD)
 
+    @Serializable
     data class AskQuestion(
         val asker: Player,
         val asked: Player,
@@ -41,23 +45,34 @@ sealed class ServerMessage(messageOrder: MessageOrder) : Message(messageOrder) {
         val isFirstQuestionInNewRound: Boolean,
     ) : ServerMessage(MessageOrder.SERVER_ASK_QUESTION)
 
+    @Serializable
     data object EndOfQuestionsRound : ServerMessage(MessageOrder.SERVER_END_OF_QUESTIONS_ROUND)
+
+    @Serializable
     data class ExtraQuestionsRound(val isExtraQuestionsRound: Boolean) :
         ServerMessage(MessageOrder.SERVER_IS_EXTRA_QUESTIONS_ROUND)
 
+    @Serializable
     data object StartVote : ServerMessage(MessageOrder.SERVER_START_VOTE)
+
+    @Serializable
     data class Imposter(val imposter: Player) : ServerMessage(MessageOrder.SERVER_IMPOSTER)
+
+    @Serializable
     data class VotingResults(val votingResults: Map<String, Int>) :
         ServerMessage(MessageOrder.SERVER_VOTING_RESULTS)
 
+    @Serializable
     data class PlayerScores(val playerScores: Map<String, Int>) :
         ServerMessage(MessageOrder.SERVER_PLAYER_SCORES)
 
+    @Serializable
     data class ReplayGame(val isReplayGame: Boolean) :
         ServerMessage(MessageOrder.SERVER_IS_REPLAY_GAME)
 }
 
-sealed class ClientMessage(messageOrder: MessageOrder) : Message(messageOrder) {
+@Serializable
+sealed class ClientMessage(private val globalOrder: MessageOrder) : Message(globalOrder) {
     data class PlayerName(val playerName: String) : ClientMessage(MessageOrder.CLIENT_PLAYER_NAME)
     data object CategoryAndWordConfirmation :
         ClientMessage(MessageOrder.CLIENT_CATEGORY_AND_WORD_CONFIRMATION)
